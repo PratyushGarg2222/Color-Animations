48f567b22d588f8f4cc414f02f232020cd344023
PratyushGarg2222 <pratyush.garg2222@gmail.com>
2020-05-24 16:02:33 +0530

    Added Base Project

---
+++ assets/js/myScript.js
@@ -0,0 +1,193 @@
+console.log("connected")
+
+var sound1 = new Howl({
+    src: ['assets/sounds/bubbles.mp3']
+  });
+
+
+  var sound2 = new Howl({
+    src: ['assets/sounds/clay.mp3']
+  });
+
+  var sound3 = new Howl({
+    src: ['assets/sounds/confetti.mp3']
+  });
+
+  var sound4 = new Howl({
+    src: ['assets/sounds/corona.mp3']
+  });
+  var sound5 = new Howl({
+    src: ['assets/sounds/dotted-spiral.mp3']
+  });
+
+  var sound6 = new Howl({
+    src: ['assets/sounds/flash-1.mp3']
+  });
+
+  var sound7 = new Howl({
+    src: ['assets/sounds/flash-2.mp3']
+  });
+
+  var sound8 = new Howl({
+    src: ['assets/sounds/flash-3.mp3']
+  });
+
+  var sound9 = new Howl({
+    src: ['assets/sounds/glimmer.mp3']
+  });
+
+  var sound10 = new Howl({
+    src: ['assets/sounds/moon.mp3']
+  });
+
+  var sound11 = new Howl({
+    src: ['assets/sounds/pinwheel.mp3']
+  });
+
+  var sound12 = new Howl({
+    src: ['assets/sounds/piston-1.mp3']
+  });
+
+  var sound13 = new Howl({
+    src: ['assets/sounds/piston-2.mp3']
+  });
+
+  var sound14 = new Howl({
+    src: ['assets/sounds/piston-3.mp3']
+  });
+
+  var sound15 = new Howl({
+    src: ['assets/sounds/prism-1.mp3']
+  });
+
+  var sound16 = new Howl({
+    src: ['assets/sounds/prism-2.mp3']
+  });
+
+  var sound17 = new Howl({
+    src: ['assets/sounds/prism-3.mp3']
+  });
+
+  var sound18 = new Howl({
+    src: ['assets/sounds/splits.mp3']
+  });
+
+  var sound19 = new Howl({
+    src: ['assets/sounds/squiggle.mp3']
+  });
+
+  var sound20 = new Howl({
+    src: ['assets/sounds/strike.mp3']
+  });
+
+  var sound21 = new Howl({
+    src: ['assets/sounds/suspension.mp3']
+  });
+
+  var sound22 = new Howl({
+    src: ['assets/sounds/timer.mp3']
+  });
+
+  var sound23 = new Howl({
+    src: ['assets/sounds/ufo.mp3']
+  });
+
+  var sound24 = new Howl({
+    src: ['assets/sounds/veil.mp3']
+  });
+
+  var sound25 = new Howl({
+    src: ['assets/sounds/wipe.mp3']
+  });
+
+  var sound26 = new Howl({
+    src: ['assets/sounds/zig-zag.mp3']
+  });
+
+ 
+document.addEventListener("keypress", function(event){
+
+    switch(event.keyCode){
+
+        case 97: sound1.play();
+            break;
+
+        case 98:  sound2.play();
+            break;
+        case 99:    sound3.play();
+                break;
+     
+                case 100:    sound4.play();
+                break;
+     
+                case 101:    sound5.play();
+                break;
+     
+                case 102:    sound6.play();
+                break;
+     
+                case 103:    sound7.play();
+                break;
+     
+                case 104:    sound8.play();
+                break;
+     
+                case 105:    sound9.play();
+                break;
+     
+                case 106:    sound10.play();
+                break;
+     
+                case 107:    sound11.play();
+                break;
+     
+                case 108:    sound12.play();
+                break;
+     
+                case 109:    sound13.play();
+                break;
+     
+                case 110:    sound14.play();
+                break;
+     
+                case 111:    sound15.play();
+                break;
+     
+                case 112:    sound16.play();
+                break;
+     
+                case 113:    sound17.play();
+                break;
+     
+                case 114:    sound18.play();
+                break;
+     
+                case 115:    sound19.play();
+                break;
+     
+                case 116:    sound20.play();
+                break;
+     
+                case 117:    sound21.play();
+                break;
+     
+                case 118:    sound22.play();
+                break;
+     
+                case 119:    sound23.play();
+                break;
+     
+                case 120:    sound24.play();
+                break;
+     
+                case 121:    sound25.play();
+                break;
+     
+                case 122:    sound26.play();
+                break;
+     
+
+
+    }
+
+})
